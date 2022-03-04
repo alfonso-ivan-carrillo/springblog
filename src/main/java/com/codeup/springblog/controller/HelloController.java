0fd89e8e99e8d98c9bac0272f7a2d77cb2a88b18
Alfonso Carrillo <alfonso.ivan.carrillo@gmail.com>
2022-03-03 20:06:24 -0600

    additional practice notes

--- src/main/java/com/codeup/springblog/controller/HelloController.java
+++ src/main/java/com/codeup/springblog/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.codeup.springblog.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -12,16 +13,34 @@ public class HelloController {
         return "<h1>Hello World!</h1>";
     }
 
-    @GetMapping("/hello/{name}")
-    @ResponseBody
-    public String helloName(@PathVariable String name){
-        return "<h1>Hello " + name + "! </h1>";
-    }
+    //using response body
+//    @GetMapping("/hello/{name}")
+//    @ResponseBody
+//    public String helloName(@PathVariable String name){
+//        return "<h1>Hello " + name + "! </h1>";
+//    }
 
     @RequestMapping(path = "/increment/{number}", method = RequestMethod.GET)
     @ResponseBody
     public String addOne(@PathVariable int number){
         return "<h1>" + number + " plus one is " + (number + 1) + "!</h1>";
+    }
+
+    @GetMapping("/hello/{name}")
+    public String sayHello(@PathVariable String name, Model model){
+        model.addAttribute("name", name);
+        return "hello";
+    }
+
+    @GetMapping("/join")
+    public String showJoinForm(){
+        return "join";
+    }
+
+    @PostMapping("/join")
+    public String joinCohort(@RequestParam(name = "cohort") String cohort, Model model){
+        model.addAttribute("cohort", "Welcome to " + cohort + "!");
+        return "join";
     }
 
 }
--- src/main/java/com/codeup/springblog/controller/HomeController.java
+++ src/main/java/com/codeup/springblog/controller/HomeController.java
@@ -12,4 +12,9 @@ public class HomeController {
     public String home(){
         return "<h1>This is the landing page!</h1>";
     }
+
+    @GetMapping("/home")
+    public String welcome(){
+        return "home";
+    }
 }
